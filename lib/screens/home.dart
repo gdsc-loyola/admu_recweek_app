d9271f80f4ff2c8ef1eb77853d737b5402d02640
Franz Taborlupa <franztaborlupa@gmail.com>
2020-08-07 02:40:21 +0800

    Org Body List Complete Data

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -42,19 +42,31 @@ class _HomeScreenState extends State<HomeScreen> {
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.all(Radius.circular(20)),
                           color: Colors.orange,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 1,
+                              blurRadius: 3,
+                              offset: Offset(0, 3), // changes position of shadow
+                            ),
+                          ],
                         ),
                         width: 144.0,
                         child: Column(
-                          
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             Padding (
                               padding: EdgeInsets.all(8.0),
+                              
                               child: Image.asset("assets/images/LIONS logo.png"),
                             ),
                             Row (
-                              
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: <Widget> [
-                                
                                 Text (
                                   '27',
                                   style: TextStyle (fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white),
@@ -74,25 +86,133 @@ class _HomeScreenState extends State<HomeScreen> {
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.all(Radius.circular(20)),
                           color: Colors.white,
+                          boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 3,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+
+                        ],
                         ),
                         width: 144.0,
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding (
+                              padding: EdgeInsets.all(8.0),
+                              
+                              child: Image.asset("assets/images/COA logo.png"),
+                            ),
+                            Row (
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget> [
+                                Text (
+                                  '56',
+                                  style: TextStyle (fontWeight: FontWeight.bold, fontSize: 16, color: Colors.black),
+                                ),
+                                Text (
+                                  ' Orgs',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle (fontSize: 12, color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                       SizedBox (width: 16),
                       Container(
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.all(Radius.circular(20)),
-                          color: Colors.green,
-                        
-                        boxShadow: [
+                          color: Colors.blueGrey,
+                          boxShadow: [
                           BoxShadow(
                             color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 7,
+                            spreadRadius: 1,
+                            blurRadius: 3,
                             offset: Offset(0, 3), // changes position of shadow
                           ),
+
                         ],
                         ),
                         width: 144.0,
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding (
+                              padding: EdgeInsets.all(8.0),
+                              child: Image.asset("assets/images/COP logos.png"),
+                            ),
+                            Row (
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget> [
+                                Text (
+                                  '3',
+                                  style: TextStyle (fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white),
+                                ),
+                                Text (
+                                  ' Orgs',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle (fontSize: 12, color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox (width: 16),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          color: Colors.blue,
+                          boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 3,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+
+                        ],
+                        ),
+                        width: 144.0,
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding (
+                              padding: EdgeInsets.all(8.0),
+                              child: Image.asset("assets/images/Student Groups logos.png"),
+                            ),
+                            Row (
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget> [
+                                Text (
+                                  '7',
+                                  style: TextStyle (fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white),
+                                ),
+                                Text (
+                                  ' Orgs',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle (fontSize: 12, color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                 ])),
               ],
