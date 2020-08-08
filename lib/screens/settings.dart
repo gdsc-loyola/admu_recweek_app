fdbce23d282844fc577a52547c6a701eca32d804
Franz Taborlupa <franztaborlupa@gmail.com>
2020-08-08 13:24:43 +0800

    Credits Widget Working

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,9 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:admu_recweek_app/models/user.dart';
-
+//import 'package:admu_recweek_app/screens/team.dart';
 // ignore: must_be_immutable
 class SettingsScreen extends StatefulWidget {
   GoogleSignIn _googleSignIn;
@@ -67,32 +65,121 @@ class _SettingsScreenState extends State<SettingsScreen> {
             Expanded (
               child: Align (
                 alignment: FractionalOffset.bottomCenter,
-                
-                child: 
-                //   Column (
-                //   children: <Widget>[
-                    
-                //   ],
-                // )
-                
-                       Column (
-                        mainAxisAlignment: MainAxisAlignment.end,
+                child: Column (
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget> [
+                    Stack (
                         children: <Widget> [
-                          Container (
-                            // margin: const EdgeInsets.only(top: 8),
-                                  decoration: BoxDecoration(
-                                    color: Colors.blue,
-                                  ),
-                                  width: double.infinity,
-                                  height: 144,
+                    //Padding (
+                        //padding: EdgeInsets.only(bottom: 48),
+                        /*child:*/ 
+                      //),
+                    Container (
+                      // margin: const EdgeInsets.only(top: 8),
+                      decoration: BoxDecoration(
+                        color: const Color(0xffFFFFFF),
+                      ),
+                      width: double.infinity,
+                      height: 168,
+                    ),
+                    Align (
+                      alignment: Alignment.bottomCenter,
+                    child: Container (
+                      margin: const EdgeInsets.only(top: 24.0),
+                          decoration: BoxDecoration(
+                            color: const Color(0xff295EFF),
+                        ),
+                    child: Container (
+                      height: 144,
+                    child: Column (
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget> [
+                        Padding (
+                          padding: EdgeInsets.only(bottom: 8.0),
+                          child: Text (
+                          "Passionately made by DSC Loyola EB 20-21",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.bold
+                            )
                           )
-                        ]
-                    
-                ),
-                //],
-              )
-            )
-            ]
-            ));
+                        ),
+                        Align (
+                          alignment: Alignment.bottomCenter,
+                          child: Container (
+                            alignment: Alignment.center,
+                            child: Row (
+                              mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget> [
+                               Padding (
+                                padding: EdgeInsets.only(right: 12.0),
+                                child: ButtonTheme(
+                                  minWidth: 120.0,
+                                  height: 40.0,
+                                  child: FlatButton (
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  
+                                  onPressed: () {
+                                    print("Product Team.");
+                                  },
+                                  color: const Color(0xff1C41B2),
+                                  child: Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Text('Product Team',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 12.0,
+                                              fontWeight: FontWeight.bold
+                                            )
+                                      )
+                                  )
+                                )
+                                )
+                                ),
+                            ButtonTheme(
+                                  minWidth: 120.0,
+                                  height: 40.0,
+                            child: FlatButton(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              onPressed: () {
+                                print("DSC Screen");
+                              },
+                              color: const Color(0xffFFFFFF),
+                              child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text('Join Us!',
+                                      style: TextStyle(
+                                          color: const Color (0xff295EFF),
+                                          fontSize: 12.0,
+                                          fontWeight: FontWeight.bold
+                                          )
+                                        )
+                                  )
+                              ) ,
+                          )]))
+                      
+                     )])))),
+                     Align (
+                       alignment: Alignment.center,
+                     child: SizedBox(
+                                height: 48,
+                                width: 48,
+                                child: Image.asset(
+                                    "assets/images/DSCL logo.png"),
+                              ))
+                    ]
+                     )
+                  ]),
+                 ),
+               )
+            ])
+            
+            );
   }
 }
