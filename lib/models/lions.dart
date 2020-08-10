a4d3532825a776cc16902cb1d8ac0edbd4124e3e
Harvey Sison <harveyjaysison@gmail.com>
2020-08-10 13:24:03 +0800

    json to list works for LIONS

--- lib/models/lions.dart
+++ lib/models/lions.dart
@@ -1,24 +1,24 @@
 class LIONS {
-  String representative;
-  String organization;
-  String orgLogoPNG;
-  String description;
-  String advocacy;
-  String vision;
-  String mission;
-  String applicationProcess;
-  String featured;
-  String photosForEachEvent;
-  String tagline;
-  String orgPhoto;
-  String benefits;
-  String flagships;
-  String departments;
-  String facebook;
-  String socMedHandles;
+  final String representative;
+  final String organization;
+  final String orgLogoPNG;
+  final String description;
+  final String advocacy;
+  final String vision;
+  final String mission;
+  final String applicationProcess;
+  final String featured;
+  final String photosForEachEvent;
+  final String tagline;
+  final String orgPhoto;
+  final String benefits;
+  final String flagships;
+  final String departments;
+  final String facebook;
+  final String socMedHandles;
 
   LIONS(
-      {this.representative,
+      this.representative,
       this.organization,
       this.orgLogoPNG,
       this.description,
@@ -34,47 +34,47 @@ class LIONS {
       this.flagships,
       this.departments,
       this.facebook,
-      this.socMedHandles});
+      this.socMedHandles);
 
-  LIONS.fromJson(Map<String, dynamic> json) {
-    representative = json['Representative'];
-    organization = json['Organization'];
-    orgLogoPNG = json['Org Logo (PNG)'];
-    description = json['Description'];
-    advocacy = json['Advocacy'];
-    vision = json['Vision'];
-    mission = json['Mission'];
-    applicationProcess = json['Application Process'];
-    featured = json['Featured'];
-    photosForEachEvent = json['Photos for each event'];
-    tagline = json['Tagline'];
-    orgPhoto = json['Org Photo'];
-    benefits = json['Benefits'];
-    flagships = json['Flagships'];
-    departments = json['Departments'];
-    facebook = json['Facebook'];
-    socMedHandles = json['SocMed Handles'];
-  }
+  // LIONS.fromJson(Map<String, dynamic> json) {
+  //   representative = json['Representative'];
+  //   organization = json['Organization'];
+  //   orgLogoPNG = json['Org Logo (PNG)'];
+  //   description = json['Description'];
+  //   advocacy = json['Advocacy'];
+  //   vision = json['Vision'];
+  //   mission = json['Mission'];
+  //   applicationProcess = json['Application Process'];
+  //   featured = json['Featured'];
+  //   photosForEachEvent = json['Photos for each event'];
+  //   tagline = json['Tagline'];
+  //   orgPhoto = json['Org Photo'];
+  //   benefits = json['Benefits'];
+  //   flagships = json['Flagships'];
+  //   departments = json['Departments'];
+  //   facebook = json['Facebook'];
+  //   socMedHandles = json['SocMed Handles'];
+  // }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['Representative'] = this.representative;
-    data['Organization'] = this.organization;
-    data['Org Logo (PNG)'] = this.orgLogoPNG;
-    data['Description'] = this.description;
-    data['Advocacy'] = this.advocacy;
-    data['Vision'] = this.vision;
-    data['Mission'] = this.mission;
-    data['Application Process'] = this.applicationProcess;
-    data['Featured'] = this.featured;
-    data['Photos for each event'] = this.photosForEachEvent;
-    data['Tagline'] = this.tagline;
-    data['Org Photo'] = this.orgPhoto;
-    data['Benefits'] = this.benefits;
-    data['Flagships'] = this.flagships;
-    data['Departments'] = this.departments;
-    data['Facebook'] = this.facebook;
-    data['SocMed Handles'] = this.socMedHandles;
-    return data;
-  }
+  // Map<String, dynamic> toJson() {
+  //   final Map<String, dynamic> data = new Map<String, dynamic>();
+  //   data['Representative'] = this.representative;
+  //   data['Organization'] = this.organization;
+  //   data['Org Logo (PNG)'] = this.orgLogoPNG;
+  //   data['Description'] = this.description;
+  //   data['Advocacy'] = this.advocacy;
+  //   data['Vision'] = this.vision;
+  //   data['Mission'] = this.mission;
+  //   data['Application Process'] = this.applicationProcess;
+  //   data['Featured'] = this.featured;
+  //   data['Photos for each event'] = this.photosForEachEvent;
+  //   data['Tagline'] = this.tagline;
+  //   data['Org Photo'] = this.orgPhoto;
+  //   data['Benefits'] = this.benefits;
+  //   data['Flagships'] = this.flagships;
+  //   data['Departments'] = this.departments;
+  //   data['Facebook'] = this.facebook;
+  //   data['SocMed Handles'] = this.socMedHandles;
+  //   return data;
+  // }
 }
--- lib/screens/list.dart
+++ lib/screens/list.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:alphabet_list_scroll_view/alphabet_list_scroll_view.dart';
-import 'package:faker/faker.dart';
+// import 'package:faker/faker.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:admu_recweek_app/models/org.dart';
+// import 'package:admu_recweek_app/models/org.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:admu_recweek_app/models/user.dart';
+import 'package:admu_recweek_app/models/lions.dart';
 
 // ignore: must_be_immutable
 class ListScreen extends StatefulWidget {
@@ -22,37 +25,136 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
-  List<User> userList = [];
+  List<LIONS> lionsList = [];
   List<String> strList = [];
   List<Widget> normalList = [];
   String sortStatus = 'Alphabetical';
 
+  // @override
+  // void initState() {
+  //   for (var i = 0; i < 100; i++) {
+  //     var name = faker.person.name();
+  //     userList.add(User(name, faker.company.name(), false));
+  //   }
+  //   userList
+  //       .sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
+  //   filterList();
+  //   widget.searchController.addListener(() {
+  //     filterList();
+  //   });
+  //   super.initState();
+  // }
   @override
   void initState() {
-    for (var i = 0; i < 100; i++) {
-      var name = faker.person.name();
-      userList.add(User(name, faker.company.name(), false));
-    }
-    userList
-        .sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
-    filterList();
-    widget.searchController.addListener(() {
-      filterList();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await loadJson();
     });
     super.initState();
   }
 
-  filterList() {
-    List<User> users = [];
-    users.addAll(userList);
-    normalList = [];
-    strList = [];
-    if (widget.searchController.text.isNotEmpty) {
-      users.retainWhere((user) => user.name
-          .toLowerCase()
-          .contains(widget.searchController.text.toLowerCase()));
+  loadJson() async {
+    var jsonResult;
+
+    // Getting the file path of the JSON and Decoding the file into String
+    String data = await rootBundle.loadString('assets/data/lions.json');
+    jsonResult = json.decode(data.toString());
+
+    // OUTPUT : [{name: Jan Salvador Sebastian, company: mclinica}, {name: Harvey sison, company: ateneo}, {name: Juan Dela Cruz, company: null universty}]
+    print(jsonResult);
+
+    // We created a loop for adding the `name` and `company` to the USER class
+    for (int i = 0; i < jsonResult.length; i++) {
+      lionsList.add(LIONS(
+          jsonResult[i]['Representative'],
+          jsonResult[i]['Organization'],
+          jsonResult[i]['Org Logo (PNG)'],
+          jsonResult[i]['Description'],
+          jsonResult[i]['Advocacy'],
+          jsonResult[i]['Vision'],
+          jsonResult[i]['Mission'],
+          jsonResult[i]['Application Process'],
+          jsonResult[i]['Featured'],
+          jsonResult[i]['Photos for each event'],
+          jsonResult[i]['Tagline'],
+          jsonResult[i]['Org Photo'],
+          jsonResult[i]['Benefits'],
+          jsonResult[i]['Flagships'],
+          jsonResult[i]['Departments'],
+          jsonResult[i]['Facebook'],
+          jsonResult[i]['SocMed Handles']));
     }
-    users.forEach((user) {
+
+    // Sorting Area
+    lionsList.sort((x, y) =>
+        x.organization.toLowerCase().compareTo(y.organization.toLowerCase()));
+
+    // Called the Filter
+    filter();
+  }
+
+  // filterList() {
+  //   List<User> users = [];
+  //   users.addAll(userList);
+  //   normalList = [];
+  //   strList = [];
+  //   if (widget.searchController.text.isNotEmpty) {
+  //     users.retainWhere((user) => user.name
+  //         .toLowerCase()
+  //         .contains(widget.searchController.text.toLowerCase()));
+  //   }
+  //   users.forEach((user) {
+  //     normalList.add(
+  //       Slidable(
+  //         actionPane: SlidableDrawerActionPane(),
+  //         actionExtentRatio: 0.25,
+  //         secondaryActions: imageUrl == ""
+  //             ? null
+  //             : <Widget>[
+  //                 IconSlideAction(
+  //                     iconWidget: Image.asset('assets/icons/list_bookmark.png'),
+  //                     onTap: () {
+  //                       Fluttertoast.showToast(
+  //                           msg: "You have bookmarked this organization",
+  //                           toastLength: Toast.LENGTH_SHORT,
+  //                           gravity: ToastGravity.BOTTOM,
+  //                           timeInSecForIosWeb: 1,
+  //                           backgroundColor: Colors.grey,
+  //                           textColor: Colors.white,
+  //                           fontSize: 16.0);
+  //                     },
+  //                     color: const Color(0xff7598FF))
+  //               ],
+  //         child: ListTile(
+  //           leading: CircleAvatar(
+  //             backgroundImage:
+  //                 NetworkImage("http://placeimg.com/200/200/people"),
+  //           ),
+  //           title: Text(user.name),
+  //           subtitle: Text(user.company),
+  //         ),
+  //       ),
+  //     );
+  //     strList.add(user.name);
+  //   });
+
+  //   setState(() {
+  //     strList;
+  //     normalList;
+  //     strList;
+  //   });
+  // }
+
+  filter() {
+    List<LIONS> orgs = [];
+    normalList = [];
+
+    // We added all the userList to the users. for the passing/getting the specific value.
+    orgs.addAll(lionsList);
+
+    // Loop
+    orgs.forEach((org) {
+      // Since, normalList is an WidgetArray = []
+      // Here is the adding of Widget that depends on the lenght of the Array in  `users`
       normalList.add(
         Slidable(
           actionPane: SlidableDrawerActionPane(),
@@ -75,22 +177,19 @@ class _ListScreenState extends State<ListScreen> {
                       color: const Color(0xff7598FF))
                 ],
           child: ListTile(
-            leading: CircleAvatar(
-              backgroundImage:
-                  NetworkImage("http://placeimg.com/200/200/people"),
-            ),
-            title: Text(user.name),
-            subtitle: Text(user.company),
+            leading: SizedBox(child: Image.asset('assets/orgs/dsc/logo.png')),
+            title: Text(org.organization),
+            subtitle: Text("LIONS"),
           ),
         ),
       );
-      strList.add(user.name);
+      strList.add(org.organization);
+      print(strList);
     });
 
+    // SetState to change the Value every time is triggers
     setState(() {
-      strList;
       normalList;
-      strList;
     });
   }
 
