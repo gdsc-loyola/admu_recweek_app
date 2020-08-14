2d875e95f31b48abd9d855ef86cc79fd78c510b6
Franz Taborlupa <franztaborlupa@gmail.com>
2020-08-14 15:08:18 +0800

    Create sanggu.dart + assets in new folder: bodies/sanggu

---
+++ lib/screens/bodies/sanggu.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:admu_recweek_app/models/user.dart';
+
+class SangguScreen extends StatefulWidget {
+  @override
+  _SangguScreenState createState() => _SangguScreenState();
+}
+
+class _SangguScreenState extends State<SangguScreen> {
+  bool bookmark = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Sanggu",
+          style: TextStyle(
+              color: const Color(0xff1C41B2), fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.05,
+        iconTheme: new IconThemeData(color: const Color(0xff1C41B2)),
+        actions: imageUrl == ""
+            ? null
+            : <Widget>[
+                Padding(
+                    padding: const EdgeInsets.only(right: 20.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          bookmark = !bookmark;
+                        });
+                      },
+                      child: SizedBox(
+                        width: 24.0,
+                        height: 24.0,
+                        child: bookmark
+                            ? Image.asset(
+                                'assets/bodies/student-groups/bookmark_active.png'
+                              )
+                            : Image.asset('assets/bodies/student-groups/bookmark.png'),
+                      ),
+                    ))
+              ],
+      ),
+      body: Container(
+          padding: const EdgeInsets.all(16.0),
+          color: Colors.white,
+          child: ListView(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.center,
+                child: SizedBox(
+                  height: 72.0,
+                  width: 72.0,
+                  child: Image.asset('assets/bodies/sanggu/logo.png'),
+                ),
+              ),
+              Text(
+                "Sanggunian ng mga Mag-aaral ng mga Paaralang Loyola ng Ateneo de Manila",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              Container(
+                  child: Image.asset('assets/bodies/sanggu/cover.png'),
+                  height: 180,
+                  margin: const EdgeInsets.only(bottom: 8, top: 16)),
+              Text(
+                "Description of Sanggu. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                style: TextStyle(fontSize: 16),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0, bottom: 16.0),
+                child: Text(
+                  "Clusters",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+                child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    onPressed: () {
+                      launch("https://lionshub.org/");
+                    },
+                    color: const Color(0xffE84C4C),
+                    child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text('Learn More',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold)))),
+              )
+            ],
+          )),
+    );
+  }
+}
