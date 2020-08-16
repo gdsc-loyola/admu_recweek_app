bdd65dea35f3b6c507665363ec75a3e52e246a75
Jan Salvador Sebastian <jansalvadorsebastian@gmail.com>
2020-08-16 16:27:32 +0800

    [Clean Up] Added Initial cleaning up (#29)

---
+++ lib/common/asset-colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// For clean up purposes...
+
+class AssetColors {
+  // TODO: You can add colors here, example below
+  //static const black = Color(0xff000000);
+  //static const ateneoBlue = Color(0xffb74093);
+
+  // MARK: How to use it.
+  // AssetColors.black
+  // AssetColors.ateneoBlue
+}
