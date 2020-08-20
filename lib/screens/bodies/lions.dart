88acae640d2320aa5b56e5128f058bde14eccd82
Franz Taborlupa <franztaborlupa@gmail.com>
2020-08-20 12:14:00 +0800

    LIONS screen data passing

--- lib/screens/bodies/lions.dart
+++ lib/screens/bodies/lions.dart
@@ -2,6 +2,9 @@ import 'dart:convert';
 import 'package:admu_recweek_app/models/orgs.dart';
 import 'package:admu_recweek_app/widgets/base-widget.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'dart:convert';
+
+import 'package:admu_recweek_app/models/orgs.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
