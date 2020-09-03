7ac6007419a8e4dca67f863ff639574aeb5e3a3e
Jan Salvador Sebastian <jansalvadorsebastian@gmail.com>
2020-09-04 01:06:46 +0800

    [Notification] Added notification

--- lib/controllers/push_nofitications.dart
+++ lib/controllers/push_nofitications.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
 
 class PushNotificationsManager {
   PushNotificationsManager._();
@@ -11,11 +12,41 @@ class PushNotificationsManager {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   bool _initialized = false;
 
-  Future<void> init() async {
+  Future<void> init(BuildContext context) async {
     if (!_initialized) {
       // For iOS request permission first.
       _firebaseMessaging.requestNotificationPermissions();
-      _firebaseMessaging.configure();
+      _firebaseMessaging.configure(
+        onMessage: (Map<String, dynamic> message) async {
+          print("message : $message");
+
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              content: ListTile(
+                title: Text(
+                  message['notification']['title'],
+                ),
+                subtitle: Text(
+                  message['notification']['body'],
+                ),
+              ),
+              actions: [
+                FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text("Close"),
+                )
+              ],
+            ),
+          );
+        },
+        onResume: (Map<String, dynamic> message) async {
+          print("message : $message");
+        },
+        onLaunch: (Map<String, dynamic> message) async {
+          print("message : $message");
+        },
+      );
 
       // For testing purposes print the Firebase Messaging token
       // String token = await _firebaseMessaging.getToken();
